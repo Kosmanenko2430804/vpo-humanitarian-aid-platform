4a8d0d1cdad91940b86b8b3b337dc4e2fa69cfd8
Анастасія Євгенівна Косманенко <2430804@stud.op.edu.ua>
2026-03-14 18:34:00 +0200

    Initial project structure: Spring Boot setup with core dependencies

---
+++ src/main/java/com/kosmanenko/vpo_humanitarian_aid_platform/VpoHumanitarianAidPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kosmanenko.vpo_humanitarian_aid_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpoHumanitarianAidPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpoHumanitarianAidPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosmanenko/vpo_humanitarian_aid_platform/VpoHumanitarianAidPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmanenko.vpo_humanitarian_aid_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpoHumanitarianAidPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
